1843abdff2a124060cfb33abd3195cf4fb36974c
Gaurav <gaurav.ps.0818@gmail.com>
2023-03-17 09:17:58 +0530

    Added Textures

--- Mesh.cpp
+++ Mesh.cpp
@@ -29,8 +29,10 @@ void Mesh::CreateMesh(GLfloat* vertices, unsigned* indices, unsigned numOfVertic
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * numOfVertices, vertices, GL_STATIC_DRAW ); // Binding Data With Buffer and we cam also use GL_DYNAMIC_DRAW
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0])*5, 0);
     glEnableVertexAttribArray(0); // which element of Array we want to start form like above example will have 9/3 = 3  ==> { 0,1,2} arrays and start from 0
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0])*5, reinterpret_cast<void*>(sizeof(vertices[0]) * 3));   // for texture
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind vertex Buffer
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);   // unbind indices(element) buffer
